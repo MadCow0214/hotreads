47370e63877a425019fa642d4d3ca77e552da253
sonhansung <sonhansung@gmail.com>
2020-03-26 23:51:24 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
 import "./passport";
-import cors from "cors";
 import { authenticateJwt } from "./passport";
 import { checkAuthenticated } from "./middlewares";
 import helmet from "helmet";
@@ -13,7 +12,6 @@ const server = new GraphQLServer({
   context: ({ request }) => ({ request, checkAuthenticated })
 });
 
-server.express.use(cors());
 server.express.use(logger("dev"));
 server.express.use(helmet());
 server.express.use(authenticateJwt);
