2674a886f632dc381f316dca097617c0c8eeeb9b
sonhansung <sonhansung@gmail.com>
2020-03-28 03:52:03 +0900

    정정

--- src/api/Author/Fields.js
+++ src/api/Author/Fields.js
@@ -2,6 +2,9 @@ import { prisma } from "../../../generated/prisma-client";
 
 export default {
   Author: {
+    books: parent => {
+      return prisma.author({ id: parent.id }).books();
+    },
     bookCount: parent => {
       return prisma
         .booksConnection({ where: { author: { id: parent.id } } })
