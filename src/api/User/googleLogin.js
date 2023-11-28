c96c246655a8ed5c4694a4ff471e0467676298cd
sonhansung <sonhansung@gmail.com>
2023-11-29 00:34:37 +0900

    Update googleLogin.js

--- src/api/User/googleLogin.js
+++ src/api/User/googleLogin.js
@@ -19,7 +19,7 @@ export default {
       console.log(tokenId);
 
       const { name, email, picture } = await axios.get(
-        `https://www.googleapis.com/oauth2/v1/userinfo?accress_token=${tokenId}`
+        `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${tokenId}`
       );
       
       let user = await prisma.user.findUnique({ 
